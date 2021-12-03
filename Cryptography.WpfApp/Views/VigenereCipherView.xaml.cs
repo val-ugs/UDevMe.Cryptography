004ce150bf3f5db6a662daed310f3c1b6ac000ff
Valugs <valentin.1999@mail.ru>
2021-12-03 23:00:30 +0300

    Added Vigenere Cipher Algorithm in WPF Application

--- Cryptography.WpfApp/Views/VigenereCipherView.xaml.cs
+++ Cryptography.WpfApp/Views/VigenereCipherView.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Cryptography.Domain.Models;
+using Cryptography.WpfApp.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,40 @@ namespace Cryptography.WpfApp.Views
         public VigenereCipherView()
         {
             InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var vigenereCipherData = new VigenereCipherData
+            {
+                Language = language1.Text,
+                Text = inputText1.Text,
+                Key = inputKey1.Text
+            };
+
+            var response = HttpClientSample.Client.PostAsJsonAsync("VigenereCipher/Encrypt", vigenereCipherData).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                outputText1.Text = response.Content.ReadAsAsync<string>().Result;
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var vigenereCipherData = new VigenereCipherData
+            {
+                Language = language2.Text,
+                Text = inputText2.Text,
+                Key = inputKey2.Text
+            };
+
+            var response = HttpClientSample.Client.PostAsJsonAsync("VigenereCipher/Decrypt", vigenereCipherData).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                outputText2.Text = response.Content.ReadAsAsync<string>().Result;
+            }
         }
     }
 }
