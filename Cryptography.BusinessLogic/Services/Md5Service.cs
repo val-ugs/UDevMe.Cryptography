ae2ac634e4b3330e7f0630f1b6000fc13f37735a
Valugs <valentin.1999@mail.ru>
2021-12-04 22:05:48 +0300

    Added verification Md5Data in Md5Service

--- Cryptography.BusinessLogic/Services/Md5Service.cs
+++ Cryptography.BusinessLogic/Services/Md5Service.cs
@@ -13,6 +13,14 @@ namespace Cryptography.BusinessLogic.Services
         private int _maxRound = 4;
         private uint _limitUInt = 16;
 
+        private Md5Data _emptyMd5Data = new Md5Data
+        {
+            A = 0,
+            B = 0,
+            C = 0,
+            D = 0
+        };
+
         public Md5Data Encrypt(Md5Data md5Data)
         {
             md5Data.Round = _maxRound;
@@ -24,8 +32,8 @@ namespace Cryptography.BusinessLogic.Services
             uint temp;
             Md5Data currentMd5Data = md5Data;
 
-            if (md5Data.Round < 0 && md5Data.Round >= _maxRound)
-                throw new Exception("No such round");
+            if (VerifyMd5Data(currentMd5Data) == false)
+                return _emptyMd5Data;
 
             for (int i = 1; i <= md5Data.Round; i++)
             {
@@ -55,8 +63,8 @@ namespace Cryptography.BusinessLogic.Services
             uint temp;
             Md5Data currentMd5Data = md5Data;
 
-            if (md5Data.Round < 0 && md5Data.Round >= _maxRound)
-                throw new Exception("No such round");
+            if (VerifyMd5Data(currentMd5Data) == false)
+                return _emptyMd5Data;
 
             for (int i = _maxRound; i > _maxRound - md5Data.Round; i--)
             {
@@ -93,7 +101,7 @@ namespace Cryptography.BusinessLogic.Services
                     f = C ^ (~D | B);
                     break;
                 default:
-                    new Exception("No sych round");
+                    new Exception("No such round");
                     break;
             }
             f %= _limitUInt;
@@ -109,6 +117,29 @@ namespace Cryptography.BusinessLogic.Services
         private uint RightRotateS(uint temp, int s)
         {
             return ((temp >> s) | (temp << (4 - s))) % _limitUInt;
+        }
+
+        private bool VerifyMd5Data(Md5Data md5Data)
+        {
+            if (VerifyUInt(md5Data.A) && VerifyUInt(md5Data.B) && VerifyUInt(md5Data.C)
+                && VerifyUInt(md5Data.D) && VerifyUInt(md5Data.M) && VerifyUInt(md5Data.K)
+                && VerifyRound(md5Data.Round))
+                return true;
+            return false;
+        }
+
+        private bool VerifyUInt(uint value)
+        {
+            return value >= 0 && value <= _limitUInt;
+        }
+
+        private bool VerifyRound(int round)
+        {
+            if (round > 0 && round <= _maxRound)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
