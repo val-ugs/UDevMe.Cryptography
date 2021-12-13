df1bf7bb8cd9fbb2f14b57f7c76b28346a3c856b
Valugs <valentin.1999@mail.ru>
2021-12-14 01:15:38 +0300

    Added Blowfish Algorithm in Wpf Application

--- Cryptography.WpfApp/ViewModels/MainWindowViewModel.cs
+++ Cryptography.WpfApp/ViewModels/MainWindowViewModel.cs
@@ -39,6 +39,7 @@ namespace Cryptography.WpfApp.ViewModels
             {
                 new Algorithm(){ Name = "A5", UserControlSample = new A5View() },
                 new Algorithm(){ Name = "Aes", UserControlSample = new AesView() },
+                new Algorithm(){ Name = "Blowfish", UserControlSample = new BlowfishView() },
                 new Algorithm(){ Name = "Caesar Cipher", UserControlSample = new CaesarCipherView() },
                 new Algorithm(){ Name = "El Gamal", UserControlSample = new ElGamalView() },
                 new Algorithm(){ Name = "Diffie Hellman", UserControlSample = new DiffieHellmanView() },
--- Cryptography.WpfApp/Views/BlowfishView.xaml.cs
+++ Cryptography.WpfApp/Views/BlowfishView.xaml.cs
@@ -19,9 +19,9 @@ using System.Windows.Shapes;
 namespace Cryptography.WpfApp.Views
 {
     /// <summary>
-    /// Interaction logic for FeistelView.xaml
+    /// Interaction logic for BlowfishView.xaml
     /// </summary>
-    public partial class FeistelView : UserControl
+    public partial class BlowfishView : UserControl
     {
         private List<Element> _inputList1;
         private List<Element> _inputList2;
@@ -33,7 +33,7 @@ namespace Cryptography.WpfApp.Views
 
         }
 
-        public FeistelView()
+        public BlowfishView()
         {
             InitializeComponent();
         }
@@ -76,14 +76,14 @@ namespace Cryptography.WpfApp.Views
         {
             Element[] elements = inputList1.Items.OfType<Element>().ToArray();
 
-            var feistelData = new FeistelData
+            var blowfishData = new BlowfishData
             {
                 L = BaseConverter.StringToUint(l1.Text, 16),
                 R = BaseConverter.StringToUint(r1.Text, 16),
                 K = elements.Select(x => BaseConverter.StringToUint(x.Value, 16)).ToList()
             };
 
-            var response = HttpClientSample.Client.PostAsJsonAsync("Feistel/Encrypt", feistelData).Result;
+            var response = HttpClientSample.Client.PostAsJsonAsync("Blowfish/Encrypt", blowfishData).Result;
 
             if (response.IsSuccessStatusCode)
             {
@@ -94,42 +94,18 @@ namespace Cryptography.WpfApp.Views
             }
         }
 
-        private void btnEncryptPerRound_Click(object sender, RoutedEventArgs e)
-        {
-            Element[] elements = inputList1.Items.OfType<Element>().ToArray();
-
-            int intValue;
-            var feistelData = new FeistelData
-            {
-                L = BaseConverter.StringToUint(l1.Text, 16),
-                R = BaseConverter.StringToUint(r1.Text, 16),
-                K = elements.Select(x => BaseConverter.StringToUint(x.Value, 16)).ToList(),
-                Round = int.TryParse(round1.Text, out intValue) ? intValue : 0
-            };
-
-            var response = HttpClientSample.Client.PostAsJsonAsync("Feistel/EncryptPerRound", feistelData).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                var result = response.Content.ReadAsAsync<FeistelData>().Result;
-
-                outputRoundL1.Text = BaseConverter.UintToString(result.L, 16);
-                outputRoundR1.Text = BaseConverter.UintToString(result.R, 16);
-            }
-        }
-
         private void btnDecrypt_Click(object sender, RoutedEventArgs e)
         {
             Element[] elements = inputList2.Items.OfType<Element>().ToArray();
 
-            var feistelData = new FeistelData
+            var blowfishData = new BlowfishData
             {
                 L = BaseConverter.StringToUint(l2.Text, 16),
                 R = BaseConverter.StringToUint(r2.Text, 16),
                 K = elements.Select(x => BaseConverter.StringToUint(x.Value, 16)).ToList()
             };
 
-            var response = HttpClientSample.Client.PostAsJsonAsync("Feistel/Decrypt", feistelData).Result;
+            var response = HttpClientSample.Client.PostAsJsonAsync("Blowfish/Decrypt", blowfishData).Result;
 
             if (response.IsSuccessStatusCode)
             {
@@ -140,28 +116,19 @@ namespace Cryptography.WpfApp.Views
             }
         }
 
-        private void btnDecryptPerRound_Click(object sender, RoutedEventArgs e)
+        private List<Element> CreateOutputList(string listName, int[] outputIntList)
         {
-            Element[] elements = inputList2.Items.OfType<Element>().ToArray();
+            List<Element> inputList = new List<Element>();
 
-            int intValue;
-            var feistelData = new FeistelData
+            for (int i = 0; i < outputIntList.Length; i++)
             {
-                L = BaseConverter.StringToUint(l2.Text, 16),
-                R = BaseConverter.StringToUint(r2.Text, 16),
-                K = elements.Select(x => BaseConverter.StringToUint(x.Value, 16)).ToList(),
-                Round = int.TryParse(round2.Text, out intValue) ? intValue : 0
-            };
-
-            var response = HttpClientSample.Client.PostAsJsonAsync("Feistel/DecryptPerRound", feistelData).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                var result = response.Content.ReadAsAsync<FeistelData>().Result;
-
-                outputRoundL2.Text = BaseConverter.UintToString(result.L, 16);
-                outputRoundR2.Text = BaseConverter.UintToString(result.R, 16);
+                Element element = new Element();
+                element.LabelName = listName + "(" + i + "):";
+                element.Value = outputIntList[i].ToString();
+                inputList.Add(element);
             }
+
+            return inputList;
         }
     }
 }
